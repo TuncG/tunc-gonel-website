a9a50fc327c17cbca437c5388768e88641aa456a
Tunc <tuncgonelfb@hotmail.com>
2022-03-11 22:33:24 -0500

    Card component complete and cards being added to the project page

---
+++ src/components/Card.js
@@ -0,0 +1,47 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Box from "@mui/material/Box";
+import Links from "@mui/material/Link";
+
+export default function MediaCard({ content }) {
+  return (
+    <Links href={content.link} underline="none">
+      <Card sx={{ maxWidth: 500, minHeight: 225, mt: 5 }} elevation={4}>
+        <CardActionArea>
+          {/* <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        /> */}
+          <Box
+            sx={{
+              height: 40,
+              backgroundColor: content.color,
+            }}
+          ></Box>
+          <CardContent sx={{ minHeight: 150 }}>
+            <Typography gutterBottom variant="h5" component="div">
+              {content.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {content.main}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Code
+          </Button>
+        </CardActions>
+      </Card>
+    </Links>
+  );
+}
+/*  Website that tracks news articles about popular tech compagnies. Made
+          with React and the Material UI library. The NewsApi api was used in
+          retreiving the data displayed. */
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,52 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Card from "../components/Card";
+import Box from "@mui/material/Box";
+
+var content = {
+  newstrack: {
+    title: "NewsTrack",
+    main: "Website that tracks news articles about popular tech compagnies. Made with React and the Material UI library. The NewsApi api was used in retreiving the data displayed",
+    color: "primary.light",
+    link: "https://github.com/TuncG/NewsTrack",
+  },
+  tuncgonel: {
+    title: "Tunc GOnel.com",
+    main: "The source code for my personal website. Made with React and styled using the Material UI library. Currently hosted on Heroku.",
+    color: "secondary.light",
+    link: "https://github.com/TuncG/tunc-gonel-website",
+  },
+  turingtest: {
+    title: "Turing Test",
+    main: "Turing test game developed in Python using the Pygame and OpenAI libraries. Questions can be inputed by the user and a response is generated using  GPT2 library from OpenAI.",
+    color: "warning.light",
+    link: "https://github.com/TuncG/tunc-gonel-website",
+  },
+};
 
 export default function Projects() {
   return (
     <Container maxWidth="lg">
-      <Typography mt={10} variant="h4">
+      <Typography mt={6} variant="h4">
         {" "}
         Projects
       </Typography>
+      <Typography mt={2} variant="body2">
+        Below you can find all my projects and a link to their code. Feel free
+        to contact me for any questions.
+      </Typography>
+
+      <Box sx={{ display: "flex" }}>
+        <Box>
+          <Card content={content.newstrack}></Card>
+          <Card content={content.tuncgonel}></Card>
+        </Box>
+
+        <Box ml={20}>
+          <Card content={content.turingtest}></Card>
+          <Card content={content.tuncgonel}></Card>
+        </Box>
+      </Box>
     </Container>
   );
 }
